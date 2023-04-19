7ba472f7a10ed6dc788ee2de5d80eafbfeb06e40
deji911 <isaacayodejiariyo@gmail.com>
2023-04-19 15:43:42 +0100

    hmmm

---
+++ src/robots.js
@@ -0,0 +1,103 @@
+import React from "react"
+export const robots = [
+    {
+    id: 1,
+    name: 'Ayodeji Ariyo',
+    username: 'deji911',
+    email: 'isaacayodejiariyo@gmail.com' 
+
+    } ,
+
+    {
+        id: 2,
+        name: 'Elizabeth Ariyo',
+        username: 'joml',
+        email: 'lizbimpyo@gmail.com' 
+    
+     } ,
+
+    {
+        id: 3,
+        name: 'Adeniyi Ariyo',
+        username: 'shola11',
+        email: 'niyi4jizzle@gmail.com' 
+        
+    } ,
+
+    {
+        id: 4,
+        name: 'Samuel OCrah',
+        username: 'sam_alx',
+        email: 'samocrah@gmail.com' 
+            
+     } ,
+
+     {
+        id: 5,
+        name: 'John Paul',
+        username: 'paul111',
+        email: 'paulJohn@gmail.com' 
+            
+     } ,
+
+     {
+        id: 6,
+        name: 'Ruth Adebayo',
+        username: 'RuthB',
+        email: 'ruthy@gmail.com' 
+                
+     } ,
+
+     {
+        id: 7,
+        name: 'Ruth Adebayo',
+        username: 'RuthB',
+        email: 'ruthy@gmail.com' 
+                
+     } ,
+
+     {
+        id: 8,
+        name: 'Emmanuel Peters',
+        username: 'RuthB',
+        email: 'Ennanuel@gmail.com' 
+                
+     } ,
+
+     {
+        id: 9,
+        name: 'Success James',
+        username: 'Sj',
+        email: 'sj@gmail.com' 
+                
+     } ,
+
+     {
+        id: 10,
+        name: 'precious James',
+        username: 'pre pre',
+        email: 'precious@gmail.com' 
+                
+     } ,
+
+     {
+      id: 11,
+      name: 'Allan Ariyo',
+      username: 'Allan',
+      email: 'allan@gmail.com' 
+              
+   } ,
+
+   {
+      id: 12,
+      name: 'Eliot Ariyo',
+      username: 'My-g',
+      email: 'eliot@gmail.com' 
+              
+   } ,
+
+
+
+
+
+]
